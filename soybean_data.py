0af07ea99dc907fa10a450d1f3a689c84f516bc8
tlarnold10 <tlarnold10@hotmail.com>
2020-05-06 22:01:19 -0500

    added a new column for a combination of the year and month

--- soybean_data.py
+++ soybean_data.py
@@ -31,10 +31,49 @@ data_dict['month'] = month_array
 data_dict['sold'] = sold_array
 weed_df = pd.DataFrame(data_dict)
 
+st.write(weed_df)
+st.write(weed_df.groupby(['month']).sum())
 
-bar_df = pd.DataFrame(weed_df[['sold', 'region']])
+# weed_df['year_month'] = str(weed_df['year']) + weed_df['month']
+# weed_df['year_month'] = weed_df.apply(lambda row: str(row['year']) + row['month'], axis=1)
 
-st.write(bar_df.plot.bar())
+year_month = []
+for index, row in weed_df.iterrows():
+    # pdb.set_trace()
+    if row.month == 'jan': 
+        year_month.append(str(row.year) + '01')
+    elif row.month == 'feb': 
+        year_month.append(str(row.year) + '02')
+    elif row.month == 'mar': 
+        year_month.append(str(row.year) + '03')
+    elif row.month == 'apr': 
+        year_month.append(str(row.year) + '04')
+    elif row.month == 'may': 
+        year_month.append(str(row.year) + '05')
+    elif row.month == 'june': 
+        year_month.append(str(row.year) + '06')
+    elif row.month == 'jul': 
+        year_month.append(str(row.year) + '07')
+    elif row.month == 'aug': 
+        year_month.append(str(row.year) + '08')
+    elif row.month == 'sep': 
+        year_month.append(str(row.year) + '09')
+    elif row.month == 'oct': 
+        year_month.append(str(row.year) + '10')
+    elif row.month == 'nov': 
+        year_month.append(str(row.year) + '11')
+    elif row.month == 'dec': 
+        year_month.append(str(row.year) + '12')
+    else:
+        print(row.month)
+weed_df['year_month'] = year_month
+
+bar_df = pd.DataFrame(weed_df[['region','sold']])
+
+# bar_df.plot.bar()
+# plot.show()
+st.bar_chart(bar_df)
+st.write(bar_df)
 
 st.title("Streamlit app about Weed")
 option = st.sidebar.multiselect(
